626c0960101db5f10364d70fccb130c0f02e2b6d
devbiplabmistry <dev.biplabmistry@gmail.com>
2023-06-07 13:43:17 +0530

    (banner text added)

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,7 +1,8 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-
 import banner1 from "../../../assets/banner/banner1.jpg"
+import banner2 from "../../../assets/banner/banner2.jpg"
+import banner6 from "../../../assets/banner/banner6.jpg"
 import banner3 from "../../../assets/banner/banner3.jpg"
 import banner4 from "../../../assets/banner/banner4.jpg"
 import banner5 from "../../../assets/banner/banner5.jpg"
@@ -13,15 +14,45 @@ const Banner = () => {
         <Carousel>
             <div>
                 <img src={banner1} />
+                <div className="absolute top-1/2 left-1/4">
+                    <h1 className="text-white font-[roboto] text-7xl font-bold mb-4 "> Mastering the Art of Dance</h1>
+                    <p className="font-[roboto] text-lg text-white">Unlock Your Potential and Move with Confidence</p>
+                </div>
             </div>
             <div>
                 <img src={banner3} />
+                <div className="absolute top-1/2 left-1/4">
+                    <h1 className="text-white font-[roboto] text-7xl font-bold mb-4 "> Journey from Beginner to Pro</h1>
+                    <p className="font-[roboto] text-lg text-white">Learn, Progress, and Shine on the Dance Floor</p>
+                </div>
             </div>
             <div>
                 <img src={banner4} />
+                <div className="absolute top-1/2 left-1/4">
+                    <h1 className="text-white font-[roboto] text-7xl font-bold mb-4 "> Discover the Magic of Dance</h1>
+                    <p className="font-[roboto] text-lg text-white">Explore Different Styles and Unleash Your Inner Dancer</p>
+                </div>
             </div>
             <div>
                 <img src={banner5} />
+                <div className="absolute top-1/2 left-1/4">
+                    <h1 className="text-white font-[roboto] text-7xl font-bold mb-4 "> Blending Styles, Unleashing Creativity</h1>
+                    <p className="font-[roboto] text-lg text-white"> Embrace Diversity and Create Unique Dance Compositions</p>
+                </div>
+            </div>
+            <div>
+                <img src={banner2} />
+                <div className="absolute top-1/2 left-1/4">
+                    <h1 className="text-white font-[roboto] text-7xl font-bold mb-4 ">Ballet Mastery for All Ages</h1>
+                    <p className="font-[roboto] text-lg text-white">Develop Poise, Strength, and Artistry through Classical Dance</p>
+                </div>
+            </div>
+            <div>
+                <img src={banner6} />
+                <div className="absolute top-1/2 left-1/4">
+                    <h1 className="text-white font-[roboto] text-7xl font-bold mb-4 ">The Beat's Guide: Hip-Hop and Street Dance Essentials</h1>
+                    <p className="font-[roboto] text-lg text-white">Master Urban Dance Moves and Command the Stage</p>
+                </div>
             </div>
 
         </Carousel>
