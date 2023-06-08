40883028e1235fd637c2a1afb40868fbe86b6287
devbiplabmistry <dev.biplabmistry@gmail.com>
2023-06-08 13:31:08 +0530

    update instructor

--- src/Pages/Home/Instructor/Instructor.jsx
+++ src/Pages/Home/Instructor/Instructor.jsx
@@ -1,11 +1,28 @@
 import useInstructor from "../../../Hooks/useInstructor";
 
 const Instructor = () => {
-    const instructor=useInstructor()
-    console.log(instructor);
+    const instructor = useInstructor()
+    const popular = instructor.filter(item => item.classesTaken >= 30)
+    console.log("instructor ", popular);
     return (
         <div>
-            
+            <h1>instructor</h1>
+            <div className="grid grid-cols-3 gap-4">
+                {
+                    popular.map(item =>
+                        <div key={item._is}>
+                            <div  className="text-center ">
+                                <img className="w-[300px] h-[300px] rounded-2xl" src={item.image} alt="" />
+                                <p className="font-[roboto] text-base font-semibold"><small>
+                                    name :{item.name}</small></p>
+                                <p className="font-[roboto]  text-base font-semibold"><small>email :{item.email}</small></p>
+                                <p className="font-[roboto]  text-base font-semibold"><small>class :{item.classesNames[0]
+                                }</small></p>
+                                <p className="font-[roboto] text-orange-500 text-base font-semibold"><small>classTaken :{item.classesTaken}</small></p>
+                            </div>
+                        </div>)
+                }
+            </div>
         </div>
     );
 };
